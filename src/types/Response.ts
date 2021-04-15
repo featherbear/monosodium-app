706a2e3131949630d6f415cad171e96d728781b0
Andrew Wong <andrew.j.wong@outlook.com>
2021-04-16 00:02:06 +1000

    Add Response generic type

---
+++ src/types/Response.ts
@@ -0,0 +1,24 @@
+export type Response<T = any> =
+  | { status: true; data?: T }
+  | { status: false; error?: string };
+
+export default Response;
+
+export function createResponse<T>(status: true, data: T) : Response<T>;
+export function createResponse(status: false, error: string): Response;
+export function createResponse<T>(
+  status: boolean,
+  data_or_error: T
+): Response<T> {
+  if (status) {
+    return {
+      status: true,
+      data: data_or_error,
+    };
+  } else {
+    return {
+      status: false,
+      error: String(data_or_error),
+    };
+  }
+}
